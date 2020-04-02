960fb74df0ef7a851c682eea15875c6a5d4809ef
quek <55364841+quek04@users.noreply.github.com>
2020-04-02 16:01:09 -0500

    i'm an idiot and forgot to actually call the registries in the main class.

--- src/main/java/bernie/software/DeepWatersMod.java
+++ src/main/java/bernie/software/DeepWatersMod.java
@@ -1,9 +1,23 @@
 package bernie.software;
 
+import bernie.software.registry.DeepWatersBiomes;
+import bernie.software.registry.DeepWatersBlocks;
+import bernie.software.registry.DeepWatersItems;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(DeepWatersMod.ModID)
 public class DeepWatersMod
 {
+
+	public DeepWatersMod() {
+		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+
+		DeepWatersBlocks.BLOCKS.register(bus);
+		DeepWatersItems.ITEMS.register(bus);
+		DeepWatersBiomes.BIOMES.register(bus);
+	}
+
 	public static final String ModID = "deepwaters";
 }
