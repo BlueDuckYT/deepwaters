3db736b0bc529830fdabdb9cbddad20e5f37fee7
BlueDuckYT <bludux595@gmail.com>
2020-04-10 18:05:47 -0400

    Added Basic NBT Tag, need help saving/loading.

--- src/main/java/bernie/software/entity/ColorfulFish.java
+++ src/main/java/bernie/software/entity/ColorfulFish.java
@@ -1,5 +1,6 @@
 package bernie.software.entity;
 
+import bernie.software.nbt.Hue;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.passive.fish.AbstractFishEntity;
 import net.minecraft.item.ItemStack;
@@ -9,13 +10,18 @@ import net.minecraft.world.World;
 public class ColorfulFish extends AbstractFishEntity
 {
 
-	String color = "";
+	Hue color;
 
 	public ColorfulFish(EntityType<? extends AbstractFishEntity> p_i50248_1_, World p_i50248_2_)
 	{
 		super(p_i50248_1_, p_i50248_2_);
 		String[] colors = {"blue", "red", "green", "purple", "yellow"};
-		color = colors[(int) (Math.random() * 5)];
+		try {
+			String str = color.getString();
+		}
+		catch (Exception e) {
+			color = new Hue(colors[(int) (Math.random() * 5)]);
+		}
 	}
 
 	@Override
@@ -30,6 +36,6 @@ public class ColorfulFish extends AbstractFishEntity
 		return null;
 	}
 	public String getColor() {
-		return color;
+		return color.getString();
 	}
 }
