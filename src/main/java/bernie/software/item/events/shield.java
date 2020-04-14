74d0140033e971926c74ef0d08164addb5465b1f
BuildTools <unconfigured@null.spigotmc.org>
2020-04-14 09:42:37 -0400

    Fixed shield in water logged blocks.

--- src/main/java/bernie/software/item/events/shield.java
+++ src/main/java/bernie/software/item/events/shield.java
@@ -10,7 +10,7 @@ public class shield {
     public static class prismarine extends useEvent {
         @Override
         public void onUse(World world, PlayerEntity playerEntity, Hand hand) {
-            if (world.getBlockState(playerEntity.getPosition()).getBlock().equals(Blocks.WATER)) {
+            if (playerEntity.isInWater()) {
                 double speedFactor=2;
                 playerEntity.setVelocity(playerEntity.getLookVec().scale(speedFactor).x,playerEntity.getLookVec().scale(speedFactor).y,playerEntity.getLookVec().scale(speedFactor).z);
             }
