c259512f14ddd9672428bdc6ae8813b2321efe8a
BuildTools <unconfigured@null.spigotmc.org>
2020-04-14 19:07:02 -0400

    Moved eel model to models

--- src/main/java/bernie/software/client/renderer/model/EelTail.java
+++ src/main/java/bernie/software/client/renderer/model/EelTail.java
@@ -1,4 +1,4 @@
-package bernie.software.client.renderer.entity;
+package bernie.software.client.renderer.model;
 import bernie.software.client.renderer.model.AbstractWormPart;
 import net.minecraft.client.renderer.entity.model.EntityModel;
 import net.minecraft.client.renderer.entity.model.RendererModel;
@@ -8,30 +8,30 @@ import net.minecraft.entity.Entity;
 // Paste this code into your mod.
 // Make sure to generate all required imports
 
-public class EelHead extends AbstractWormPart {
-	private final RendererModel eel3;
+public class EelTail extends AbstractWormPart {
+	private final RendererModel eel1;
 
 	@Override
 	public RendererModel getModel() {
-		return eel3;
+		return eel1;
 	}
 
-	public EelHead() {
+	public EelTail() {
 		textureWidth = 32;
 		textureHeight = 16;
 
-		eel3 = new RendererModel(this);
-		eel3.setRotationPoint(-0, 0, 0);
-		eel3.cubeList.add(new ModelBox(eel3, 18, 0, -0F, 0.5F, -2.136f, 0, 1, 2, 0.0F, false));
-		eel3.cubeList.add(new ModelBox(eel3, 18, 0, -0F, -1.5F, -2.136f, 0, 1, 2, 0.0F, false));
-		eel3.cubeList.add(new ModelBox(eel3, 13, 0, -0.5F, -0.5F, -3.136f, 1, 1, 3, 0.0F, false));
-		eel3.offsetZ=0;
-//		eel3.offsetX=1f;
+		eel1 = new RendererModel(this);
+		eel1.setRotationPoint(-0, 0, 0);
+		eel1.cubeList.add(new ModelBox(eel1, 17, 4, -0.0F, 0.5F, 1.8675f, 0, 1, 2, 0.0F, false));
+		eel1.cubeList.add(new ModelBox(eel1, 17, 4, -0.0F, -1.5f, 1.8675f, 0, 1, 2, 0.0F, false));
+		eel1.cubeList.add(new ModelBox(eel1, 7, 0, -0.5F, -0.5F, 0.8675f, 1, 1, 4, 0.0F, false));
+		eel1.offsetX=1f;
 	}
 
 	@Override
 	public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {
-		eel3.render(f5);
+
+		eel1.render(f5);
 	}
 	public void setRotationAngle(RendererModel modelRenderer, float x, float y, float z) {
 		modelRenderer.rotateAngleX = x;
