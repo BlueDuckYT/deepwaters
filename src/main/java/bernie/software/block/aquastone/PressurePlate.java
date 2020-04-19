cd0c8587a669d76d624b08e446fc43b4d1a7e7ed
GiantLuigi4 <49770992+giantluigi4@users.noreply.github.com>
2020-04-18 21:05:26 -0400

    Added waterlogged stone button, added translations to all aquastone stuff except the button as that's gonna change, aquastone torches are now waterloggable. #MINOR

---
+++ src/main/java/bernie/software/block/aquastone/PressurePlate.java
@@ -0,0 +1,17 @@
+package bernie.software.block.aquastone;
+
+import net.minecraft.block.*;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.fluid.IFluidState;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.BooleanProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IWorld;
+import net.minecraft.world.World;
+
+public class PressurePlate {
+}
--- src/main/java/bernie/software/block/aquastone/RedstoneTorch.java
+++ src/main/java/bernie/software/block/aquastone/RedstoneTorch.java
@@ -1,12 +1,16 @@
 package bernie.software.block.aquastone;
 
 import bernie.software.registry.DeepWatersBlocks;
-import net.minecraft.block.Block;
-import net.minecraft.block.RedstoneTorchBlock;
-import net.minecraft.block.RedstoneWallTorchBlock;
+import net.minecraft.block.*;
 import net.minecraft.block.material.Material;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.fluid.IFluidState;
+import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.item.Item;
 import net.minecraft.item.WallOrFloorItem;
+import net.minecraft.state.BooleanProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
 
 public class RedstoneTorch {
     public static class Item extends WallOrFloorItem {
@@ -17,12 +21,50 @@ public class RedstoneTorch {
             super(DeepWatersBlocks.AQUA_TORCH.get(),DeepWatersBlocks.AQUA_TORCH_WALL.get(),propertiesIn);
         }
     }
-    public static class AquastoneTorch extends RedstoneTorchBlock {
+    public static class AquastoneTorch extends RedstoneTorchBlock implements IWaterLoggable {
+        public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
+
+        @Override
+        protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder)
+        {
+            builder.add(LIT, WATERLOGGED);
+        }
+
+        @Override
+        public BlockState getStateForPlacement(BlockItemUseContext context)
+        {
+            return super.getStateForPlacement(context).with(WATERLOGGED,false);
+        }
+
+        public IFluidState getFluidState(BlockState state)
+        {
+            return state.get(WATERLOGGED) ? Fluids.WATER.getStillFluidState(false) : super.getFluidState(state);
+        }
+
         public AquastoneTorch() {
             super(Block.Properties.create(Material.ROCK));
         }
     }
-    public static class AquastoneTorchWall extends RedstoneWallTorchBlock {
+    public static class AquastoneTorchWall extends RedstoneWallTorchBlock implements IWaterLoggable {
+        public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
+
+        @Override
+        protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder)
+        {
+            builder.add(FACING, REDSTONE_TORCH_LIT, WATERLOGGED);
+        }
+
+        @Override
+        public BlockState getStateForPlacement(BlockItemUseContext context)
+        {
+            return super.getStateForPlacement(context).with(WATERLOGGED,false);
+        }
+
+        public IFluidState getFluidState(BlockState state)
+        {
+            return state.get(WATERLOGGED) ? Fluids.WATER.getStillFluidState(false) : super.getFluidState(state);
+        }
+
         public AquastoneTorchWall() {
             super(Block.Properties.create(Material.ROCK));
         }
