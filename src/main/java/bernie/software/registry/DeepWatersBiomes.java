fc6fa34dd045b8dbc63c7338d551ecf286299690
Bernie-G <berniegandin@gmail.com>
2020-04-26 18:48:38 -0400

    feat(biome) Added thick kelp forest all the way

--- src/main/java/bernie/software/registry/DeepWatersBiomes.java
+++ src/main/java/bernie/software/registry/DeepWatersBiomes.java
@@ -19,11 +19,14 @@ public class DeepWatersBiomes
 			"coralfields", () -> new CoralFieldsBiome());
 	public static final RegistryObject<Biome> SunkenWastesBiome = BIOMES.register(
 			"sunkenwastes", () -> new SunkenWastesBiome());
-	public static final RegistryObject<Biome> ThickKelpForst = BIOMES.register(
+	public static final RegistryObject<Biome> ThickKelpForest = BIOMES.register(
 			"thickkelpforest", () -> new ThickKelpForest());
 
 	public static void addBiomeTypes()
 	{
 		BiomeDictionary.addTypes(CoralFieldsBiome.get(), BiomeDictionary.Type.OCEAN);
+		BiomeDictionary.addTypes(SunkenWastesBiome.get(), BiomeDictionary.Type.OCEAN);
+		BiomeDictionary.addTypes(ThickKelpForest.get(), BiomeDictionary.Type.OCEAN);
+
 	}
 }
