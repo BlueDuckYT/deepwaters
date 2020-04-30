e26d0cd5446aa9c9445c9c5d57dbe4ed073f6cee
Bernie-G <berniegandin@gmail.com>
2020-04-30 00:08:34 -0400

    feat(entity): Added shark

--- src/main/java/bernie/software/client/renderer/model/SharkModel.java
+++ src/main/java/bernie/software/client/renderer/model/SharkModel.java
@@ -8,6 +8,9 @@ import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.vertex.IVertexBuilder;
 import net.minecraft.client.renderer.entity.model.EntityModel;
 import net.minecraft.client.renderer.model.ModelRenderer;
+import net.minecraft.util.math.MathHelper;
+
+import static java.lang.Math.abs;
 
 public class SharkModel extends EntityModel<Shark>
 {
@@ -16,7 +19,6 @@ public class SharkModel extends EntityModel<Shark>
 	private final ModelRenderer Neck2;
 	private final ModelRenderer Head;
 	private final ModelRenderer Jaw;
-	private final ModelRenderer bone;
 	private final ModelRenderer Body;
 	private final ModelRenderer LPectoralFin;
 	private final ModelRenderer LPectoralFin2;
@@ -33,140 +35,160 @@ public class SharkModel extends EntityModel<Shark>
 	private final ModelRenderer TailFinUp;
 	private final ModelRenderer TailFinDown;
 
-	public SharkModel() {
+	public SharkModel()
+	{
 		textureWidth = 128;
 		textureHeight = 128;
 
 		Main = new ModelRenderer(this);
 		Main.setRotationPoint(-0.1F, 15.0F, 0.0F);
 
-
 		Neck = new ModelRenderer(this);
 		Neck.setRotationPoint(-0.004F, -6.63F, -6.55F);
 		Main.addChild(Neck);
-		Neck.setTextureOffset(24, 48).addBox(-3.93F, 0.175F, -5.67F, 8.0F, 7.0F, 6.0F, 0.0F, false);
+		Neck.setTextureOffset(24, 48).addBox(-3.93F, 0.425F, -5.67F, 8.0F, 7.0F, 6.0F, 0.0F, false);
 
 		Neck2 = new ModelRenderer(this);
 		Neck2.setRotationPoint(0.044F, 0.0F, -5.59F);
 		Neck.addChild(Neck2);
-		Neck2.setTextureOffset(54, 31).addBox(-3.004F, 1.22F, -4.095F, 5.0F, 5.0F, 4.0F, 0.0F, false);
+		Neck2.setTextureOffset(54, 31).addBox(-3.004F, 1.56F, -4.095F, 6.0F, 5.0F, 4.0F, 0.0F, false);
 
 		Head = new ModelRenderer(this);
-		Head.setRotationPoint(0.044F, 0.0F, -9.605F);
+		Head.setRotationPoint(0.044F, 4.0F, -9.605F);
 		Neck.addChild(Head);
-		Head.setTextureOffset(0, 53).addBox(-3.004F, 1.95F, -8.11F, 5.0F, 4.0F, 8.0F, 0.0F, false);
-		Head.setTextureOffset(70, 18).addBox(-2.665F, 5.33F, -7.885F, 5.0F, 1.0F, 6.0F, 0.0F, false);
+		Head.setTextureOffset(0, 53).addBox(-2.504F, -2.035F, -8.11F, 5.0F, 4.0F, 8.0F, 0.0F, false);
+		Head.setTextureOffset(69, 17).addBox(-2.265F, 1.54F, -7.585F, 0.0F, 1.0F, 7.0F, 0.0F, false);
+		Head.setTextureOffset(64, 10).addBox(-2.015F, 1.54F, -7.835F, 4.0F, 1.0F, 0.0F, 0.0F, false);
+		Head.setTextureOffset(69, 17).addBox(2.235F, 1.54F, -7.585F, 0.0F, 1.0F, 7.0F, 0.0F, false);
 
 		Jaw = new ModelRenderer(this);
-		Jaw.setRotationPoint(-0.1168F, 6.2958F, -0.4124F);
+		Jaw.setRotationPoint(-0.1168F, 2.2958F, -0.4124F);
 		Head.addChild(Jaw);
 		setRotationAngle(Jaw, 0.3491F, 0.0F, 0.0F);
-		Jaw.setTextureOffset(56, 0).addBox(-2.5222F, -0.7231F, -5.164F, 5.0F, 1.0F, 5.0F, 0.0F, false);
-		Jaw.setTextureOffset(64, 10).addBox(-2.3972F, -1.1731F, -5.014F, 4.0F, 1.0F, 5.0F, 0.0F, false);
-
-		bone = new ModelRenderer(this);
-		bone.setRotationPoint(-0.3222F, -0.5931F, -5.1515F);
-		Jaw.addChild(bone);
-		setRotationAngle(bone, 0.0F, 1.5708F, 0.0F);
-
+		Jaw.setTextureOffset(56, 0).addBox(-1.7722F, -0.498F, -5.0785F, 4.0F, 1.0F, 5.0F, 0.0F, false);
+		Jaw.setTextureOffset(64, 10).addBox(-1.6472F, -1.198F, -4.9285F, 0.0F, 1.0F, 5.0F, 0.0F, false);
+		Jaw.setTextureOffset(64, 10).addBox(2.1028F, -1.198F, -4.9285F, 0.0F, 1.0F, 5.0F, 0.0F, false);
+		Jaw.setTextureOffset(64, 10).addBox(-1.7222F, -1.198F, -4.9285F, 4.0F, 1.0F, 0.0F, 0.0F, false);
 
 		Body = new ModelRenderer(this);
 		Body.setRotationPoint(0.04F, -6.33F, -1.46F);
 		Main.addChild(Body);
-		Body.setTextureOffset(0, 21).addBox(-5.16F, 0.06F, -4.42F, 10.0F, 7.0F, 11.0F, 0.0F, false);
+		Body.setTextureOffset(0, 21).addBox(-4.91F, 0.06F, -4.42F, 10.0F, 7.0F, 11.0F, 0.0F, false);
 		Body.setTextureOffset(0, 0).addBox(-4.39F, -1.21F, -4.92F, 9.0F, 9.0F, 12.0F, 0.0F, false);
 
 		LPectoralFin = new ModelRenderer(this);
-		LPectoralFin.setRotationPoint(5.311F, 3.9505F, 0.5F);
+		LPectoralFin.setRotationPoint(4.811F, 3.9505F, 0.5F);
 		Body.addChild(LPectoralFin);
 		setRotationAngle(LPectoralFin, 0.0F, 0.0F, 0.6981F);
-		LPectoralFin.setTextureOffset(31, 21).addBox(-0.9155F, 0.6042F, -3.96F, 9.0F, 2.0F, 8.0F, 0.0F, false);
+		LPectoralFin.setTextureOffset(31, 21).addBox(-0.5325F, 0.8378F, -3.96F, 9.0F, 2.0F, 8.0F, 0.0F, false);
 
 		LPectoralFin2 = new ModelRenderer(this);
-		LPectoralFin2.setRotationPoint(-2.6636F, -1.2576F, -0.5F);
+		LPectoralFin2.setRotationPoint(7.9694F, 1.671F, 0.75F);
 		LPectoralFin.addChild(LPectoralFin2);
 		setRotationAngle(LPectoralFin2, 0.0F, -0.3491F, 0.0F);
-		LPectoralFin2.setTextureOffset(40, 48).addBox(9.102F, 3.2781F, -3.956F, 10.0F, 0.0F, 6.0F, 0.0F, false);
+		LPectoralFin2.setTextureOffset(40, 48).addBox(-1.2993F, 0.0281F, -2.5642F, 10.0F, 0.0F, 6.0F, 0.0F, false);
 
 		RPectoralFin = new ModelRenderer(this);
-		RPectoralFin.setRotationPoint(-4.141F, 3.9505F, 0.5F);
+		RPectoralFin.setRotationPoint(-5.141F, 5.4505F, 0.5F);
 		Body.addChild(RPectoralFin);
 		setRotationAngle(RPectoralFin, 0.0F, 0.0F, -0.6981F);
-		RPectoralFin.setTextureOffset(31, 21).addBox(-9.0706F, -0.0386F, -3.96F, 9.0F, 2.0F, 8.0F, 0.0F, true);
+		RPectoralFin.setTextureOffset(31, 21).addBox(-7.3404F, 0.0101F, -3.96F, 9.0F, 2.0F, 8.0F, 0.0F, true);
 
 		RPectoralFin2 = new ModelRenderer(this);
-		RPectoralFin2.setRotationPoint(4.0966F, 0.1739F, -0.5F);
+		RPectoralFin2.setRotationPoint(-5.6732F, 0.9176F, 0.75F);
 		RPectoralFin.addChild(RPectoralFin2);
 		setRotationAngle(RPectoralFin2, 0.0F, 0.3491F, 0.0F);
-		RPectoralFin2.setTextureOffset(40, 48).addBox(-21.4449F, 1.2038F, -4.6381F, 10.0F, 0.0F, 6.0F, 0.0F, true);
+		RPectoralFin2.setTextureOffset(40, 48).addBox(-9.869F, -0.0462F, -2.8188F, 10.0F, 0.0F, 6.0F, 0.0F, true);
 
 		FinDorsal = new ModelRenderer(this);
 		FinDorsal.setRotationPoint(-0.02F, -1.15F, 0.365F);
 		Body.addChild(FinDorsal);
 		setRotationAngle(FinDorsal, -0.0873F, 0.0F, 0.0F);
-		FinDorsal.setTextureOffset(56, 10).addBox(-0.94F, -1.435F, -2.7942F, 1.0F, 3.0F, 6.0F, 0.0F, false);
+		FinDorsal.setTextureOffset(56, 10).addBox(-0.94F, -1.2831F, -2.8378F, 2.0F, 3.0F, 7.0F, 0.0F, false);
 
 		FinDorsal2 = new ModelRenderer(this);
 		FinDorsal2.setRotationPoint(0.75F, -1.3355F, 0.7821F);
 		FinDorsal.addChild(FinDorsal2);
 		setRotationAngle(FinDorsal2, -0.1745F, 0.0F, 0.0F);
-		FinDorsal2.setTextureOffset(57, 19).addBox(-1.25F, -2.4412F, -2.6191F, 1.0F, 4.0F, 5.0F, 0.0F, false);
+		FinDorsal2.setTextureOffset(57, 19).addBox(-1.25F, -2.0612F, -2.6191F, 1.0F, 4.0F, 5.0F, 0.0F, false);
 
 		FinDorsal3 = new ModelRenderer(this);
 		FinDorsal3.setRotationPoint(0.0F, -2.287F, 0.6881F);
 		FinDorsal2.addChild(FinDorsal3);
 		setRotationAngle(FinDorsal3, -0.5236F, 0.0F, 0.0F);
-		FinDorsal3.setTextureOffset(0, 36).addBox(-0.73F, -5.3212F, -1.7724F, 0.0F, 7.0F, 3.0F, 0.0F, false);
+		FinDorsal3.setTextureOffset(0, 36).addBox(-0.73F, -5.0212F, -1.7724F, 0.0F, 7.0F, 3.0F, 0.0F, false);
 
 		Tail = new ModelRenderer(this);
 		Tail.setRotationPoint(0.04F, -2.83F, 5.63F);
 		Main.addChild(Tail);
 
-
 		BodyTail = new ModelRenderer(this);
-		BodyTail.setRotationPoint(0.25F, -3.5F, -1.25F);
+		BodyTail.setRotationPoint(0.0F, 0.25F, 0.0F);
 		Tail.addChild(BodyTail);
-		BodyTail.setTextureOffset(0, 39).addBox(-4.245F, -0.24F, 0.92F, 8.0F, 7.0F, 7.0F, 0.0F, false);
+		BodyTail.setTextureOffset(0, 39).addBox(-3.995F, -3.325F, -0.33F, 8.0F, 7.0F, 7.0F, 0.0F, false);
 
 		BodyTail2 = new ModelRenderer(this);
-		BodyTail2.setRotationPoint(0.25F, -3.5F, 6.05F);
+		BodyTail2.setRotationPoint(-0.25F, 0.5F, 6.8F);
 		Tail.addChild(BodyTail2);
-		BodyTail2.setTextureOffset(52, 54).addBox(-3.88F, 0.125F, 0.92F, 7.0F, 6.0F, 5.0F, 0.0F, false);
+		BodyTail2.setTextureOffset(52, 54).addBox(-3.38F, -2.94F, -0.33F, 7.0F, 6.0F, 5.0F, 0.0F, false);
 
 		BodyTail3 = new ModelRenderer(this);
-		BodyTail3.setRotationPoint(0.25F, -3.5F, 11.89F);
+		BodyTail3.setRotationPoint(-0.25F, 0.5F, 11.39F);
 		Tail.addChild(BodyTail3);
-		BodyTail3.setTextureOffset(31, 31).addBox(-3.515F, 0.49F, 0.92F, 6.0F, 6.0F, 11.0F, 0.0F, false);
+		BodyTail3.setTextureOffset(31, 31).addBox(-2.89F, -2.305F, -0.08F, 6.0F, 5.0F, 11.0F, 0.0F, false);
 
 		TailFin = new ModelRenderer(this);
-		TailFin.setRotationPoint(0.0F, 0.0F, 10.51F);
+		TailFin.setRotationPoint(0.05F, 0.0F, 10.01F);
 		BodyTail3.addChild(TailFin);
-		TailFin.setTextureOffset(0, 0).addBox(-2.055F, -0.605F, -0.175F, 3.0F, 9.0F, 3.0F, 0.0F, false);
+		TailFin.setTextureOffset(0, 0).addBox(-1.43F, -4.365F, -0.675F, 3.0F, 9.0F, 3.0F, 0.0F, false);
 
 		TailFinUp = new ModelRenderer(this);
-		TailFinUp.setRotationPoint(5.11F, -3.285F, 0.365F);
+		TailFinUp.setRotationPoint(0.11F, -3.035F, 1.365F);
 		TailFin.addChild(TailFinUp);
 		setRotationAngle(TailFinUp, -0.6981F, 0.0F, 0.0F);
-		TailFinUp.setTextureOffset(31, 17).addBox(-5.18F, -5.5799F, 1.3583F, 0.0F, 8.0F, 4.0F, 0.0F, false);
+		TailFinUp.setTextureOffset(31, 17).addBox(-0.055F, -7.65F, -2.3619F, 0.0F, 8.0F, 4.0F, 0.0F, false);
 
 		TailFinDown = new ModelRenderer(this);
-		TailFinDown.setRotationPoint(5.11F, 5.135F, 0.34F);
+		TailFinDown.setRotationPoint(0.06F, 3.635F, 1.34F);
 		TailFin.addChild(TailFinDown);
 		setRotationAngle(TailFinDown, 0.6981F, 0.0F, 0.0F);
-		TailFinDown.setTextureOffset(0, 17).addBox(-5.18F, 2.5189F, -2.7821F, 0.0F, 8.0F, 4.0F, 0.0F, false);
+		TailFinDown.setTextureOffset(0, 17).addBox(-0.005F, -0.5219F, -2.1636F, 0.0F, 8.0F, 4.0F, 0.0F, false);
 	}
 
 	@Override
-	public void setRotationAngles(Shark entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch){
-		//previously the render function, render code was moved to a method below
+	public void setRotationAngles(Shark entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch)
+	{
+		this.Tail.rotateAngleY = MathHelper.sin(limbSwing * 0.2F) * 0.3F;
+		this.BodyTail3.rotateAngleY = MathHelper.sin(limbSwing * 0.2F) * 0.3F;
+		this.TailFin.rotateAngleY = MathHelper.sin(limbSwing * 0.2F) * 0.3F;
+
+		this.FinDorsal2.rotateAngleZ = MathHelper.sin(limbSwing * 0.2F) * 0.3F;
+		this.FinDorsal3.rotateAngleZ = MathHelper.sin(limbSwing * 0.2F) * 0.3F;
+
+		this.LPectoralFin.rotateAngleZ = abs(MathHelper.sin(limbSwing * 0.2F) * 0.7f);
+		this.LPectoralFin2.rotateAngleZ = abs(MathHelper.sin(limbSwing * 0.2F) * 0.7f);
+		this.RPectoralFin.rotateAngleZ = -1 * abs(MathHelper.sin(limbSwing * 0.2F) * 0.7f);
+		this.RPectoralFin2.rotateAngleZ = -1 * abs(MathHelper.sin(limbSwing * 0.2F) * 0.7f);
 	}
 
 	@Override
-	public void render(MatrixStack matrixStack, IVertexBuilder buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha){
+	public void setLivingAnimations(Shark entityIn, float limbSwing, float limbSwingAmount, float partialTick)
+	{
+		if(entityIn.isAttacking() || (this.Jaw.rotateAngleX >= -6.1F && this.Jaw.rotateAngleX <= -5.9))
+		{
+			this.Jaw.rotateAngleX = -1 * abs(MathHelper.sin(limbSwing * 0.2F)) * 0.9F + 0.5F;
+		}
+		super.setLivingAnimations(entityIn, limbSwing, limbSwingAmount, partialTick);
+	}
+
+	@Override
+	public void render(MatrixStack matrixStack, IVertexBuilder buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha)
+	{
 		Main.render(matrixStack, buffer, packedLight, packedOverlay);
 	}
 
-	public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
+	public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z)
+	{
 		modelRenderer.rotateAngleX = x;
 		modelRenderer.rotateAngleY = y;
 		modelRenderer.rotateAngleZ = z;
--- src/main/java/bernie/software/entity/Shark.java
+++ src/main/java/bernie/software/entity/Shark.java
@@ -1,16 +1,59 @@
 package bernie.software.entity;
 
+import bernie.software.entity.ai.goal.UnderwaterCreatureAttackGoal;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.controller.MovementController;
+import net.minecraft.entity.ai.goal.NearestAttackableTargetGoal;
+import net.minecraft.entity.ai.goal.RandomSwimmingGoal;
+import net.minecraft.entity.ai.goal.ZombieAttackGoal;
+import net.minecraft.entity.monster.DrownedEntity;
 import net.minecraft.entity.passive.fish.AbstractFishEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.network.datasync.DataParameter;
+import net.minecraft.network.datasync.DataSerializers;
+import net.minecraft.network.datasync.EntityDataManager;
 import net.minecraft.util.SoundEvent;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
 
 public class Shark extends AbstractFishEntity
 {
+	private static final DataParameter<Boolean> IS_ATTACKING = EntityDataManager.createKey(Shark.class, DataSerializers.BOOLEAN);
+
+
 	public Shark(EntityType<? extends AbstractFishEntity> type, World worldIn)
 	{
 		super(type, worldIn);
+		this.moveController = new MoveHelperController(this);
+	}
+
+	/**
+	 * Sets the active target the Task system uses for tracking
+	 *
+	 * @param entitylivingbaseIn
+	 */
+	@Override
+	public void setAttackTarget(@Nullable LivingEntity entitylivingbaseIn)
+	{
+		super.setAttackTarget(entitylivingbaseIn);
+		this.dataManager.set(IS_ATTACKING, entitylivingbaseIn != null);
+	}
+
+	protected void registerData()
+	{
+		super.registerData();
+		this.dataManager.register(IS_ATTACKING, false);
+	}
+
+	public boolean isAttacking()
+	{
+		return this.dataManager.get(IS_ATTACKING);
 	}
 
 	@Override
@@ -23,5 +66,81 @@ public class Shark extends AbstractFishEntity
 	protected SoundEvent getFlopSound()
 	{
 		return null;
+	}
+
+	@Override
+	protected void registerAttributes()
+	{
+		super.registerAttributes();
+		this.getAttribute(SharedMonsterAttributes.FOLLOW_RANGE).setBaseValue(16.0D);
+		this.getAttribute(SharedMonsterAttributes.FOLLOW_RANGE).setBaseValue(20.0D);
+
+		this.getAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(40.0D);
+		this.getAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(1.0D);
+		this.getAttributes().registerAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(3);
+	}
+
+	@Override
+	protected void registerGoals() {
+		this.targetSelector.addGoal(0, new NearestAttackableTargetGoal(this, PlayerEntity.class, true));
+		this.goalSelector.addGoal(0, new UnderwaterCreatureAttackGoal(this, 3.0D, false));
+		this.goalSelector.addGoal(4, new SwimGoal(this));
+
+	}
+
+	@Override
+	protected void updateAITasks()
+	{
+		super.updateAITasks();
+		this.setHomePosAndDistance(new BlockPos(this), 50);
+	}
+
+	static class MoveHelperController extends MovementController
+	{
+		private final Shark shark;
+
+		public MoveHelperController(Shark p_i48909_1_) {
+			super(p_i48909_1_);
+			this.shark = p_i48909_1_;
+		}
+
+		public void tick() {
+			if (this.shark.isInWater()) {
+				if (this.action != MovementController.Action.MOVE_TO || this.shark.getNavigator().noPath()) {
+					this.shark.setAIMoveSpeed(0.0F);
+					return;
+				}
+
+				double d0 = this.posX - this.shark.getPosX();
+				double d1 = this.posY - this.shark.getPosY();
+				double d2 = this.posZ - this.shark.getPosZ();
+				double d3 = (double) MathHelper.sqrt(d0 * d0 + d1 * d1 + d2 * d2);
+				d1 = d1 / d3;
+				float f = (float)(MathHelper.atan2(d2, d0) * (double)(180F / (float)Math.PI)) - 90.0F;
+				this.shark.rotationYaw = this.limitAngle(this.shark.rotationYaw, f, 90.0F);
+				this.shark.renderYawOffset = this.shark.rotationYaw;
+				float f1 = (float)(this.speed * this.shark.getAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).getValue());
+				float f2 = MathHelper.lerp(0.125F, this.shark.getAIMoveSpeed(), f1);
+				this.shark.setAIMoveSpeed(f2);
+				this.shark.setMotion(this.shark.getMotion().add((double)f2 * d0 * 0.005D, (double)f2 * d1 * 0.1D, (double)f2 * d2 * 0.005D));
+			} else {
+				if (!this.shark.onGround) {
+					this.shark.setMotion(this.shark.getMotion().add(0.0D, -0.008D, 0.0D));
+				}
+
+				super.tick();
+			}
+
+		}
+	}
+	static class SwimGoal extends RandomSwimmingGoal
+	{
+		private final AbstractFishEntity fish;
+
+		public SwimGoal(AbstractFishEntity fish) {
+			super(fish, 1.0D, 40);
+			this.fish = fish;
+		}
+
 	}
 }
