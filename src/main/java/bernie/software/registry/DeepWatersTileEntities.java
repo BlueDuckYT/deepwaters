accffe667446fa4b8fcb80b2e7761186720c0aae
GiantLuigi4 <49770992+giantluigi4@users.noreply.github.com>
2020-04-30 20:42:01 -0400

    smth smth fan.

--- src/main/java/bernie/software/registry/DeepWatersTileEntities.java
+++ src/main/java/bernie/software/registry/DeepWatersTileEntities.java
@@ -15,7 +15,7 @@ import java.util.function.Supplier;
 
 public class DeepWatersTileEntities {
     public static final DeferredRegister<TileEntityType<?>> TILE_ENTITIES = new DeferredRegister<>(ForgeRegistries.TILE_ENTITIES, DeepWatersMod.ModID);
-    public static final RegistryObject<TileEntityType<TileEntityAquaFan>> AQUASTONE_FAN = TILE_ENTITIES.register("aquastone_fan", () -> TileEntityType.Builder.create(
+    public static final RegistryObject<TileEntityType<?>> AQUASTONE_FAN = TILE_ENTITIES.register("aquastone_fan", () -> TileEntityType.Builder.create(
             TileEntityAquaFan::new, DeepWatersBlocks.AQUA_FAN.get()).build(null));
 
 }
