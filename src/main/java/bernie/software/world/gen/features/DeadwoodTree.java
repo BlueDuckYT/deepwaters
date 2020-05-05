7d9a02864224dc4933ed08210e41987a04d6de47
recon <recongamingsam@gmail.com>
2020-05-05 13:15:01 +1000

    Improved deadwood tree generation

--- src/main/java/bernie/software/world/gen/features/DeadwoodTree.java
+++ src/main/java/bernie/software/world/gen/features/DeadwoodTree.java
@@ -5,6 +5,7 @@ import com.mojang.datafixers.Dynamic;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IWorld;
 import net.minecraft.world.gen.ChunkGenerator;
@@ -23,27 +24,58 @@ public class DeadwoodTree extends Feature {
 
     @Override
     public boolean place(IWorld worldIn, ChunkGenerator generator, Random rand, BlockPos pos, IFeatureConfig config) {
-        int i = 0;
-        int j = worldIn.getHeight(Heightmap.Type.OCEAN_FLOOR, pos.getX(), pos.getZ());
-        BlockPos blockpos = new BlockPos(pos.getX(), j, pos.getZ());
+        int worldInHeight = worldIn.getHeight(Heightmap.Type.OCEAN_FLOOR, pos.getX(), pos.getZ());
+        BlockPos blockpos = new BlockPos(pos.getX(), worldInHeight, pos.getZ());
 
         Block deadWoodLog = DeepWatersBlocks.DEADWOOD_LOG.get();
 
         if (worldIn.getBlockState(blockpos).getBlock() == Blocks.WATER &&
-                !(worldIn.getBlockState(blockpos.down()).getBlock() == deadWoodLog )) {
+                !(worldIn.getBlockState(blockpos.down()).getBlock() == deadWoodLog)) {
             BlockState blockstate = DeepWatersBlocks.DEADWOOD_LOG.get().getDefaultState();
-            int k = 1 + rand.nextInt(7);
-//
-//            if (rand.nextInt(1000) <= 1) {
-//                worldIn.setBlockState(pos.down(), DeepWatersBlocks.AQUASTONE_ORE.get().getDefaultState(), 2);
-//            }
+            int treeHeight = 1 + rand.nextInt(7);
 
-            for (int l = 0; l <= k; ++l) {
+            for (int l = 0; l <= treeHeight; ++l) {
                 if (worldIn.getBlockState(blockpos).getBlock() == Blocks.WATER && worldIn.getBlockState(blockpos.up()).getBlock() == Blocks.WATER && blockstate.isValidPosition(worldIn, blockpos)) {
-                    if (l == k) {
+                    if (l == treeHeight) {
+                        int height = rand.nextInt(3) + rand.nextInt(3) + 5;
+
+                        Direction direction = Direction.Plane.HORIZONTAL.random(rand);
+                        int k2 = height - rand.nextInt(4) - 1;
+                        int l2 = 3 - rand.nextInt(3);
+                        int i3 = pos.getX();
+                        int j3 = pos.getZ();
+
+                        for (int k1 = 0; k1 < height; ++k1) {
+                            int l1 = pos.getY() + k1;
+                            if (k1 >= k2 && l2 > 0) {
+                                i3 += direction.getXOffset();
+                                j3 += direction.getZOffset();
+                                --l2;
+                            }
+                            BlockPos blockpos2 = new BlockPos(i3, l1, j3);
+                            worldIn.setBlockState(blockpos2, deadWoodLog.getDefaultState(), 2);
+                        }
+
+                        Direction direction1 = Direction.Plane.HORIZONTAL.random(rand);
+                        if (direction1 != direction) {
+                            int i4 = k2 - rand.nextInt(2) - 1;
+                            int l4 = 1 + rand.nextInt(3);
+
+                            for (int i2 = i4; i2 < height && l4 > 0; --l4) {
+                                if (i2 >= 1) {
+                                    int j2 = pos.getY() + i2;
+                                    i3 += direction1.getXOffset();
+                                    j3 += direction1.getZOffset();
+                                    BlockPos blockpos1 = new BlockPos(i3, j2, j3);
+                                    worldIn.setBlockState(blockpos1, deadWoodLog.getDefaultState(), 2);
+                                }
+
+                                ++i2;
+                            }
+                        }
+
 
                         worldIn.setBlockState(blockpos, deadWoodLog.getDefaultState(), 2);
-                        ++i;
                     } else {
                         worldIn.setBlockState(blockpos, deadWoodLog.getDefaultState(), 2);
                     }
@@ -51,7 +83,6 @@ public class DeadwoodTree extends Feature {
                     BlockPos blockpos1 = blockpos.down();
                     if (blockstate.isValidPosition(worldIn, blockpos1) && worldIn.getBlockState(blockpos1.down()).getBlock() != blockstate.getBlock()) {
                         worldIn.setBlockState(blockpos, deadWoodLog.getDefaultState(), 2);
-                        ++i;
                     }
                     break;
                 }
@@ -60,8 +91,10 @@ public class DeadwoodTree extends Feature {
             }
         }
 
-        return i > 0;
+        return true;
     }
+
+
 }
 
 
